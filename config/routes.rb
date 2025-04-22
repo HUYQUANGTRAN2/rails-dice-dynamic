9bd3fd3cc30558a7f025f7f4d598f7f19cb5193e
loganoppenheime <64404164+HUYQUANGTRAN2@users.noreply.github.com>
2025-04-22 06:25:29 +0000

    quang's 1st commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-
+  get("/",{:controller =>"homepage",:action =>"displaying"})
+  get("dice/:number_of_dice/:how_many_sides",{:controller => "rolling", :action => "dynamic"}) 
 end
